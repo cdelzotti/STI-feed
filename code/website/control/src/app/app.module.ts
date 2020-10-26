1547bc052cb78870a157db609f852c42aeb13dee
Clement Delzotti <clement.delzotti@student.unamur.be>
2020-10-26 18:08:57 +0100

    Adding edition dialog box

--- code/website/control/src/app/app.module.ts
+++ code/website/control/src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule, MatDialogContent, MatDialogActions} from "@angular/material/dialog"
+import { FormsModule } from '@angular/forms'
+
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component'
-import { EventListComponent } from './event-list/event-list.component'
+import { EventListComponent, EventListEditDialog } from './event-list/event-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    EventListComponent
+    EventListComponent,
+    EventListEditDialog
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     MatIconModule,
     HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
